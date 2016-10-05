0aba84afe61123595acf9d86b6ee2156386f2611
freek <freek@spatie.be>
2016-10-05 14:57:02 +0200

    wip

--- config/laravel-db-language-lines.php
+++ config/laravel-db-language-lines.php
@@ -3,8 +3,8 @@
 return [
 
     /**
-     * The model that handles the language lines.
+     * The model that handles the language lines. You can place any model here
+     * that extends Spatie\DbLanguageLines\LanguageLine.
      */
     'model' => Spatie\DbLanguageLines\LanguageLine::class,
-
 ];
