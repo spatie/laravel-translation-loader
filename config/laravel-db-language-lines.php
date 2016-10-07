304d9e865e3875c483546fbaf2102d93effc5c71
Freek Van der Herten <freek@spatie.be>
2016-10-07 12:14:39 +0000

    Applied fixes from StyleCI

--- config/laravel-db-language-lines.php
+++ config/laravel-db-language-lines.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /**
+    /*
      * Language lines will be fetched by these loaders. You can put any class here that implements
      * the Spatie\DbLanguageLines\TranslationLoaders\TranslationLoader-interface.
      */
@@ -10,7 +10,7 @@ return [
         Spatie\DbLanguageLines\TranslationLoaders\Db::class,
     ],
 
-    /**
+    /*
      * This is the model used by the Db Translation loader. You can put any model here
      * that extends Spatie\DbLanguageLines\LanguageLine.
      */
--- tests/DummyLoader.php
+++ tests/DummyLoader.php
@@ -6,7 +6,6 @@ use Spatie\DbLanguageLines\TranslationLoaders\TranslationLoader;
 
 class DummyLoader implements TranslationLoader
 {
-
     public function loadTranslations(string $locale, string $group, string $namespace = null): array
     {
         return ['dummy' => 'this is dummy'];
