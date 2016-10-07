42c40a0eaa770bfc651843cafa86b236443df688
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-07 15:16:05 +0200

    Register before boot

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -8,6 +8,16 @@ use Illuminate\Translation\TranslationServiceProvider as IlluminateTranslationSe
 class TranslationServiceProvider extends IlluminateTranslationServiceProvider
 {
     /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        parent::register();
+
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-translation-loader.php', 'laravel-translation-loader');
+    }
+
+    /**
      * Bootstrap the application services.
      */
     public function boot()
@@ -16,8 +26,8 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
             $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
             $this->publishes([
-               __DIR__.'/../config/laravel-translation-loader.php' => config_path('laravel-translation-loader.php'),
-           ], 'config');
+                __DIR__.'/../config/laravel-translation-loader.php' => config_path('laravel-translation-loader.php'),
+            ], 'config');
 
             if (! class_exists('CreateLanguageLinesTable')) {
                 $timestamp = date('Y_m_d_His', time());
@@ -28,16 +38,6 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
                 ], 'migrations');
             }
         }
-    }
-
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        parent::register();
-
-        $this->mergeConfigFrom(__DIR__.'/../config/laravel-translation-loader.php', 'laravel-translation-loader');
     }
 
     /**
