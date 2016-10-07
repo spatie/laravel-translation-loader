6b7cbcbc9d3c535dc7c10eacd3efe8d884d7aa55
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-07 16:21:07 +0200

    Documented TranslationLoader

--- src/TranslationLoaders/TranslationLoader.php
+++ src/TranslationLoaders/TranslationLoader.php
@@ -4,8 +4,13 @@ namespace Spatie\TranslationLoader\TranslationLoaders;
 
 interface TranslationLoader
 {
-    /*
+    /**
      * Returns all translations for the given locale and group.
+     *
+     * @param string $locale
+     * @param string $group
+     *
+     * @return array
      */
     public function loadTranslations(string $locale, string $group): array;
 }
