4116859207b1d09943448db9a28a91f67f35f739
freek <freek@spatie.be>
2016-10-07 14:14:29 +0200

    wip

--- config/laravel-db-language-lines.php
+++ config/laravel-db-language-lines.php
@@ -3,11 +3,16 @@
 return [
 
     /**
-     * Language lines will be fetched by these loaders.
+     * Language lines will be fetched by these loaders. You can put any class here that implements
+     * the Spatie\DbLanguageLines\TranslationLoaders\TranslationLoader-interface.
      */
     'translationLoaders' => [
         Spatie\DbLanguageLines\TranslationLoaders\Db::class,
     ],
 
+    /**
+     * This is the model used by the Db Translation loader. You can put any model here
+     * that extends Spatie\DbLanguageLines\LanguageLine.
+     */
     'model' => Spatie\DbLanguageLines\LanguageLine::class,
 ];
