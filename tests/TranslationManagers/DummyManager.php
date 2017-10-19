ba739c9e5c81910638ce07c973d0cea2a829f06e
Benjamin Michotte <bmichotte@gmail.com>
2017-10-19 09:21:35 +0200

    Allow to configure and override TranslationLoaderManager
    Signed-off-by: Benjamin Michotte <bmichotte@gmail.com>

---
+++ tests/TranslationManagers/DummyManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Spatie\TranslationLoader\Test\TranslationManagers;
+
+use Spatie\TranslationLoader\TranslationLoaderManager;
+
+class DummyManager extends TranslationLoaderManager
+{
+
+}
