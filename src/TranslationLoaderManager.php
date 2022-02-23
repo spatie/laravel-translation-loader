5916b203e115709b2823d0b91d0ef235d1174ba3
dmason30 <danmason@spendology.com>
2022-02-23 14:18:48 +0000

    Prevent unnecessary query call when model exists

--- src/TranslationLoaderManager.php
+++ src/TranslationLoaderManager.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\TranslationLoader;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Support\Facades\Schema;
 use Spatie\TranslationLoader\TranslationLoaders\TranslationLoader;
@@ -19,23 +20,27 @@ class TranslationLoaderManager extends FileLoader
      */
     public function load($locale, $group, $namespace = null): array
     {
-        $modelClass = config('translation-loader.model');
-        $model = new $modelClass;
-        if (is_a($model, LanguageLine::class)) {
-            if (! Schema::hasTable($model->getTable())) {
-                return parent::load($locale, $group, $namespace);
+        try {
+            $fileTranslations = parent::load($locale, $group, $namespace);
+
+            if (!is_null($namespace) && $namespace !== '*') {
+                return $fileTranslations;
             }
+
+            $loaderTranslations = $this->getTranslationsForTranslationLoaders($locale, $group, $namespace);
+
+            return array_replace_recursive($fileTranslations, $loaderTranslations);
+        } catch (QueryException $e) {
+            $modelClass = config('translation-loader.model');
+            $model = new $modelClass;
+            if (is_a($model, LanguageLine::class)) {
+                if (! Schema::hasTable($model->getTable())) {
+                    return parent::load($locale, $group, $namespace);
+                }
+            }
+
+            throw $e;
         }
-
-        $fileTranslations = parent::load($locale, $group, $namespace);
-
-        if (! is_null($namespace) && $namespace !== '*') {
-            return $fileTranslations;
-        }
-
-        $loaderTranslations = $this->getTranslationsForTranslationLoaders($locale, $group, $namespace);
-
-        return array_replace_recursive($fileTranslations, $loaderTranslations);
     }
 
     protected function getTranslationsForTranslationLoaders(
