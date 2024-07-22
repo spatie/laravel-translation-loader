5cce48ec8a463f593503ee1bc69b44b49fe9f05a
alex <aleksandar.naydenov@delta.bg>
2024-07-22 13:46:36 +0300

    removed constants and comments in test files

--- tests/Feature/JsonTransTest.php
+++ tests/Feature/JsonTransTest.php
@@ -2,45 +2,40 @@
 
 declare(strict_types=1);
 
-const TERM1 = 'file not found';
-
-const TERM1_EN = 'File not found';
-
-const TERM1_NL = 'Bestand niet gevonden';
-
-const TERM1_EN_DB = 'File not found from db';
-
-const TERM2 = 'file not found. it might be in trash.';
-
-const TERM2_EN = 'File not found. It might be in trash.';
-
-const TERM2_NL = 'Bestand niet gevonden. Het bestand is waarschijnlijk verwijderd.';
-
-const TERM2_EN_DB = 'File not found from db. It might be in trash.';
+beforeEach(function () {
+    $this->term1 = 'file not found';
+    $this->term1En = 'File not found';
+    $this->term1Nl = 'Bestand niet gevonden';
+    $this->term1EnDb = 'File not found from db';
+    $this->term2 = 'file not found. it might be in trash.';
+    $this->term2En = 'File not found. It might be in trash.';
+    $this->term2Nl = 'Bestand niet gevonden. Het bestand is waarschijnlijk verwijderd.';
+    $this->term2EnDb = 'File not found from db. It might be in trash.';
+});
 
 it('can get translations for language files', function () {
-    expect(__(TERM1))->toEqual(TERM1_EN)
-        ->and(__(TERM2))->toEqual(TERM2_EN);
+    expect(__($this->term1))->toEqual($this->term1En)
+        ->and(__($this->term2))->toEqual($this->term2En);
 });
 
 it('can get translations for language files for the current locale', function () {
     app()->setLocale('nl');
 
-    expect(__(TERM1))->toEqual(TERM1_NL)
-        ->and(__(TERM2))->toEqual(TERM2_NL);
+    expect(__($this->term1))->toEqual($this->term1Nl)
+        ->and(__($this->term2))->toEqual($this->term2Nl);
 });
 
 test('by default it will prefer a db translation over a file translation', function () {
-    createLanguageLine('*', TERM1, ['en' => TERM1_EN_DB]);
-    createLanguageLine('*', TERM2, ['en' => TERM2_EN_DB]);
+    createLanguageLine('*', $this->term1, ['en' => $this->term1EnDb]);
+    createLanguageLine('*', $this->term2, ['en' => $this->term2EnDb]);
 
-    expect(__(TERM1))->toEqual(TERM1_EN_DB)
-        ->and(__(TERM2))->toEqual(TERM2_EN_DB);
+    expect(__($this->term1))->toEqual($this->term1EnDb)
+        ->and(__($this->term2))->toEqual($this->term2EnDb);
 });
 
 it('will default to fallback if locale is missing', function () {
     app()->setLocale('de');
-    createLanguageLine('*', TERM1, ['en' => TERM1_EN_DB]);
+    createLanguageLine('*', $this->term1, ['en' => $this->term1EnDb]);
 
-    expect(__(TERM1))->toEqual(TERM1_EN_DB);
+    expect(__($this->term1))->toEqual($this->term1EnDb);
 });
