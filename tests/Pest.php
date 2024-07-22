e03d3965e354b5cc598aa05ec9a85e03c0ac628e
alex <aleksandar.naydenov@delta.bg>
2024-07-22 13:47:39 +0300

    removed DOCBlocks form Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,21 +7,11 @@ use Tests\TestCase;
 
 uses(TestCase::class)->in('Feature');
 
-/**
- * @return void
- */
 function flushIlluminateTranslatorCache(): void
 {
     app('translator')->setLoaded([]);
 }
 
-/**
- * @param  string  $group
- * @param  string  $key
- * @param  array  $text
- *
- * @return LanguageLine
- */
 function createLanguageLine(string $group, string $key, array $text): LanguageLine
 {
     return LanguageLine::create(compact('group', 'key', 'text'));
